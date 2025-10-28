647dd0f0cf652b45c15ae9cc1abd5855cb436eaa
L4U04 <r3tr01012@gmail.com>
2025-10-28 11:37:52 -0300

    fixing logic issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+export const prisma = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma;
+}
